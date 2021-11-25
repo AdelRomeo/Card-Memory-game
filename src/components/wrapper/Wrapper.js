49429e6edff22b93b02c04c1dd224abbd0789e79
AdelRomeo <11cage11@mail.ru>
2021-11-26 00:39:26 +0700

    body is ready

--- src/components/wrapper/Wrapper.js
+++ src/components/wrapper/Wrapper.js
@@ -9,10 +9,10 @@ export function Wrapper() {
   const arr = [fa0, fa1, fa2, fa3, fa4, fa5, fa6, fa7, fa8, fa9, fa0, fa1, fa2, fa3, fa4, fa5, fa6, fa7, fa8, fa9]
   //перемешанный массив картинок
   const [listCards, setListCards] = useState(arr)
-  //выбранные картинки
-  const [listActiveCards, setListActiveCards] = useState([])
-  //результат карточек
-  const [resultsFlips, setResultsFlip] = useState(false)
+  //открытые карточки
+  const [activeCards, setActiveCard] = useState([])
+  //список совпавших карточек
+  const [openCards, setOpenCards] = useState([])
 
   //случайный порядок карточек
   const shuffleArray = (array) => {
@@ -33,33 +33,54 @@ export function Wrapper() {
     createCardList()
   }, [])
 
-  // useEffect(() => {
-  //   getCheckSumCard(listActiveCards)
-  // }, [listActiveCards])
+  useEffect(()=>{
+    //первая карточка по которой кликнули
+    let firstActiveCard = listCards[activeCards[0]]
+    //вторая карточка по которой кликнули
+    let secondActiveCard = listCards[activeCards[1]]
 
-  const addActiveCard = (id) => {
-    setListActiveCards(listActiveCards => listActiveCards.concat(id))
-  }
-
-  const getCheckSumCard = (list) => {
-    let check = false
-    if (list.length > 1) {
-      if (list[0] === list[1]) {
-        check = true
-      }
+    //если карточек 2 и они одинаковые
+    if (secondActiveCard && firstActiveCard === secondActiveCard) {
+      //добавляем в активные карточки первую карточку по которой кликнули
+      setOpenCards(openCards => [...openCards, firstActiveCard, secondActiveCard])
     }
-    return check
+
+    //если 2 или больше карточек - очищаем список отрытых карточек
+    if (activeCards.length > 1) {
+      setTimeout(()=>{
+        setActiveCard([])
+      }, 1500)
+    }
+
+  } , [activeCards])
+
+  //клик по карточке
+  const onHandleCard = (idCard) => {
+    //добавляем в список открытых карточек, карточку по которой кликнули
+    setActiveCard(activeCards => [...activeCards, idCard])
+    console.log(openCards)
+    if (openCards.length === listCards.length) {
+      console.log('game over')
+    }
   }
 
   return (
     <article className='Wrapper'>
-      {getCheckSumCard(listActiveCards)}
-      {listCards.map((item, index) => {
-        return (
-          <Card key={index} addActiveCard={addActiveCard} index={item} listActiveCards={listActiveCards}>
-            <FontAwesomeIcon icon={listCards[index]}/>
-          </Card>
-        )
+      {listCards.map((item, i) => {
+        let isFlip = false
+        //если карточка та по которой кликнули
+        if (activeCards.includes(i)) isFlip = true
+        //если карточка из списка совпаших
+        if (openCards.includes(item)) isFlip = true
+          return (
+            <div
+              key={i}
+              className={`Card ${isFlip ? 'Card-roll' : ''}`}
+              onClick={() => onHandleCard(i)}
+            >
+              <FontAwesomeIcon icon={listCards[i]}/>
+            </div>
+          )
       })}
     </article>
   )
