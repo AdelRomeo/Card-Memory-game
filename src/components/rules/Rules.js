7b42cbe8c7abf0fcd4c4c3309467c9b58e979d9c
AdelRomeo <11cage11@mail.ru>
2021-12-13 00:53:06 +0700

    adaptive

--- src/components/rules/Rules.js
+++ src/components/rules/Rules.js
@@ -5,10 +5,17 @@ export function Rules() {
   //показ правил игры
   const [showRules, setShowRules] = useState(false)
 
+  const toggleVisibleRules = () => {
+    setShowRules(true)
+    setTimeout(()=> {
+      setShowRules(false)
+    }, 2500)
+  }
+
   return (
     <div className='rules-container' style={{top: showRules ? '0px' : '-57px'}}>
       <p className='rules-descr'>Click on cards to find duplicate images</p>
-      <button className='rules-show' onClick={()=>setShowRules(showRules => !showRules)}>rules</button>
+      <button className='rules-show' onClick={toggleVisibleRules}>rules</button>
     </div>
   )
 }
